14b3a2962d2290d0a34093484ad03a64cb85ed04
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-04-20 05:56:37 -0700

    util: add utility functions for money calculations
    See discussion: https://github.com/killbill/killbill/issues/494.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

---
+++ src/main/java/org/killbill/billing/plugin/util/KillBillMoney.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Groupon, Inc
+ * Copyright 2016 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.billing.plugin.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.killbill.billing.catalog.api.Currency;
+
+public abstract class KillBillMoney {
+
+    private KillBillMoney() {}
+
+    public static BigDecimal of(final BigDecimal amount, final Currency currency) {
+        final CurrencyUnit currencyUnit = CurrencyUnit.getInstance(currency.toString());
+        return amount.setScale(currencyUnit.getDecimalPlaces(), BigDecimal.ROUND_HALF_UP);
+    }
+
+    public static long toMinorUnits(final String currencyIsoCode, final BigDecimal amountBD) {
+        return toMinorUnits(currencyIsoCode, amountBD, RoundingMode.HALF_UP);
+    }
+
+    public static long toMinorUnits(final String currencyIsoCode, final BigDecimal amountBD, final RoundingMode roundingMode) {
+        // The payment amount specified in minor units, without the decimal separator
+        final CurrencyUnit currencyUnit = CurrencyUnit.of(currencyIsoCode);
+        return Money.of(currencyUnit, amountBD, roundingMode).getAmountMinorLong();
+    }
+}
---
+++ src/test/java/org/killbill/billing/plugin/util/TestKillBillMoney.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Groupon, Inc
+ * Copyright 2016 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.billing.plugin.util;
+
+import java.math.BigDecimal;
+
+import org.killbill.billing.catalog.api.Currency;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestKillBillMoney {
+
+    @Test(groups = "fast")
+    public void testOf() {
+        Assert.assertEquals(KillBillMoney.of(new BigDecimal("10.000000000"), Currency.EUR), new BigDecimal("10.00"));
+        Assert.assertEquals(KillBillMoney.of(new BigDecimal("10.000000000"), Currency.GBP), new BigDecimal("10.00"));
+        Assert.assertEquals(KillBillMoney.of(new BigDecimal("10.000000000"), Currency.JPY), new BigDecimal("10"));
+    }
+
+    @Test(groups = "fast")
+    public void testToMinorUnits() {
+        Assert.assertEquals(KillBillMoney.toMinorUnits("USD", BigDecimal.ZERO), 0L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("USD", BigDecimal.ONE), 100L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("USD", new BigDecimal("0.99")), 99L);
+        // See https://github.com/killbill/killbill/issues/494
+        Assert.assertEquals(KillBillMoney.toMinorUnits("USD", new BigDecimal("0.999")), 100L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("USD", BigDecimal.TEN), 1000L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("EUR", new BigDecimal("10.000000000")), 1000L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("GBP", new BigDecimal("10.000000000")), 1000L);
+        Assert.assertEquals(KillBillMoney.toMinorUnits("JPY", new BigDecimal("10.000000000")), 10);
+    }
+}
